4d584f94b911a7c04f7c297b971d8c12c56b89bd
Abuzar Siddiqui <abuzar.22scse1420076@galgotiasuniversity.edu.in>
2026-05-02 03:00:57 +0530

    updated api for deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://team-task-manager-production-320b.up.railway.app/api",
 });
 
 API.interceptors.request.use((req) => {
